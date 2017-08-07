8cfef5462105e867d882da484f3f2f20ecce709d
cryinlaugh <cryinlaugh@gmail.com>
2017-08-07 18:06:02 +0800

    add sw memcpy of double and float. add the forgotten slave file of conv pad. by yx.

--- swdnn_test/src/test_swdnn_main.cpp
+++ swdnn_test/src/test_swdnn_main.cpp
@@ -2,6 +2,7 @@ extern "C" {
 #include "caffe/swlayers/sw_conv_layer_impl.h"
 #include "caffe/swlayers/sw_relu_layer_impl.h"
 #include "caffe/util/data_type_trans.h"
+#include "caffe/util/sw_memcpy.h"
 #include "athread.h"
 }
 #include "caffe/swlayers/conv_layer_impl.hpp"
@@ -1248,6 +1249,124 @@ void test_float2double(int count,const char* n) {
 #undef DST_TYPE
 }
 
+void test_sw_memcpy_double(int count,const char* n) {
+#define SRC_TYPE double
+#define DST_TYPE double
+  printf("Test double memcpy...\n");
+  //int count = 32*1024*1024; // 8*32M double data;
+  SRC_TYPE* src = (SRC_TYPE*)malloc(count*sizeof(SRC_TYPE));
+  DST_TYPE* dst = (DST_TYPE*)malloc(count*sizeof(DST_TYPE));
+  DST_TYPE* dst_ref = (DST_TYPE*)malloc(count*sizeof(DST_TYPE));
+  printf("creating data... SIZE:%d\n",count);
+  for( int i = 0; i < count; ++i )
+    src[i] = rand()/(SRC_TYPE)RAND_MAX - 0.5;
+
+  printf("calling sw_double2float.\n");
+  char str1[100] = "Accelerated double memcpy ";
+  strcat(str1,n);
+  begin_timer(str1);
+  sw_memcpy_d(src,dst,count);
+  stop_timer();
+
+  printf("calculating reference value.\n");
+  char str2[100] = "Reference double memcpy ";
+  strcat(str2,n);
+  begin_timer(str2);
+  for( int i = 0; i < count; ++i )
+    dst_ref[i] = (DST_TYPE)src[i];
+  stop_timer();
+
+  printf("calculating errors...\n");
+  float sum=0.0, sum_ref=0.0;
+  printf("inout at 0: In:%lf Ref:%f vs SW:%f\n",src[0],dst_ref[0],dst[0]);
+  for( int i = 0; i < count; ++i ) {
+    sum+=dst[i];
+    sum_ref+=dst_ref[i];
+    if(fabs(dst_ref[i] - dst[i])>1e-4) {
+      printf("WRONG at %d: Ref:%lf vs SW:%lf\n",i,dst_ref[i],dst[i]);
+      printf("\tIn:%lf\n",src[i]);
+      printf("************* memcpy_d FAILED ***************");
+      free(src);
+      free(dst);
+      free(dst_ref);
+      return ;
+    }
+  }
+  if(fabs(sum_ref - sum)>1e-4) {
+      printf("WRONG at SUM: Ref:%lf vs SW:%lf\n",sum_ref,sum);
+      printf("************* memcpy_d FAILED ***************");
+      free(src);
+      free(dst);
+      free(dst_ref);
+      return ;
+  }
+  printf("memcpy_d is OK.");
+  free(src);
+  free(dst);
+  free(dst_ref);
+#undef SRC_TYPE
+#undef DST_TYPE
+}
+
+void test_sw_memcpy_float(int count,const char* n) {
+#define SRC_TYPE float
+#define DST_TYPE float
+  printf("Test float memcpy...\n");
+  //int count = 32*1024*1024; // 8*32M double data;
+  SRC_TYPE* src = (SRC_TYPE*)malloc(count*sizeof(SRC_TYPE));
+  DST_TYPE* dst = (DST_TYPE*)malloc(count*sizeof(DST_TYPE));
+  DST_TYPE* dst_ref = (DST_TYPE*)malloc(count*sizeof(DST_TYPE));
+  printf("creating data... SIZE:%d\n",count);
+  for( int i = 0; i < count; ++i )
+    src[i] = rand()/(SRC_TYPE)RAND_MAX - 0.5;
+
+  printf("calling sw_double2float.\n");
+  char str1[100] = "Accelerated float memcpy ";
+  strcat(str1,n);
+  begin_timer(str1);
+  sw_memcpy_f(src,dst,count);
+  stop_timer();
+
+  printf("calculating reference value.\n");
+  char str2[100] = "Reference float memcpy ";
+  strcat(str2,n);
+  begin_timer(str2);
+  for( int i = 0; i < count; ++i )
+    dst_ref[i] = (DST_TYPE)src[i];
+  stop_timer();
+
+  printf("calculating errors...\n");
+  float sum=0.0, sum_ref=0.0;
+  printf("inout at 0: In:%lf Ref:%f vs SW:%f\n",src[0],dst_ref[0],dst[0]);
+  for( int i = 0; i < count; ++i ) {
+    sum+=dst[i];
+    sum_ref+=dst_ref[i];
+    if(fabs(dst_ref[i] - dst[i])>1e-4) {
+      printf("WRONG at %d: Ref:%lf vs SW:%lf\n",i,dst_ref[i],dst[i]);
+      printf("\tIn:%lf\n",src[i]);
+      printf("************* memcpy_f FAILED ***************");
+      free(src);
+      free(dst);
+      free(dst_ref);
+      return ;
+    }
+  }
+  if(fabs(sum_ref - sum)>1e-4) {
+      printf("WRONG at SUM: Ref:%lf vs SW:%lf\n",sum_ref,sum);
+      printf("************* memcpy_f FAILED ***************");
+      free(src);
+      free(dst);
+      free(dst_ref);
+      return ;
+  }
+  printf("memcpy_f is OK.");
+  free(src);
+  free(dst);
+  free(dst_ref);
+#undef SRC_TYPE
+#undef DST_TYPE
+}
+
 #define TEST_SIZE_1  64*1024
 #define TEST_SIZE_2  128*1024
 #define TEST_SIZE_3  256*1024
@@ -1262,16 +1381,16 @@ void test_float2double(int count,const char* n) {
 int main() {
   athread_init();
   //test_forward_pad();
-  test_forward_pad_float();
-  for(int i = 0; i < 10; ++i)
-    test_forward_pad_float_fast();
+  //test_forward_pad_float();
+  //for(int i = 0; i < 10; ++i)
+  //  test_forward_pad_float_fast();
 
   //test_backward_pad();
   //test_backward_pad_float();
   //test_backward_pad_float_fast();
   //test_backward_pad_split_float_fast();
-  for(int i = 0; i < 10; ++i)
-    test_backward_pad_split();
+  //for(int i = 0; i < 10; ++i)
+  //  test_backward_pad_split();
 
   //test_backward();
   //test_forward();
@@ -1304,6 +1423,28 @@ int main() {
   //test_float2double(TEST_SIZE_9,"9");
   //test_double2float(TEST_SIZE_10,"10");
   //test_float2double(TEST_SIZE_10,"10");
+  //
+  test_sw_memcpy_double(TEST_SIZE_1,"1");
+  test_sw_memcpy_float (TEST_SIZE_1,"1");
+  test_sw_memcpy_double(TEST_SIZE_2,"2");
+  test_sw_memcpy_float (TEST_SIZE_2,"2");
+  test_sw_memcpy_double(TEST_SIZE_3,"3");
+  test_sw_memcpy_float (TEST_SIZE_3,"3");
+  test_sw_memcpy_double(TEST_SIZE_4,"4");
+  test_sw_memcpy_float (TEST_SIZE_4,"4");
+  test_sw_memcpy_double(TEST_SIZE_5,"5");
+  test_sw_memcpy_float (TEST_SIZE_5,"5");
+  test_sw_memcpy_double(TEST_SIZE_6,"6");
+  test_sw_memcpy_float (TEST_SIZE_6,"6");
+  test_sw_memcpy_double(TEST_SIZE_7,"7");
+  test_sw_memcpy_float (TEST_SIZE_7,"7");
+  test_sw_memcpy_double(TEST_SIZE_8,"8");
+  test_sw_memcpy_float (TEST_SIZE_8,"8");
+  test_sw_memcpy_double(TEST_SIZE_9,"9");
+  test_sw_memcpy_float (TEST_SIZE_9,"9");
+  test_sw_memcpy_double(TEST_SIZE_10,"10");
+  test_sw_memcpy_float (TEST_SIZE_10,"10");
+
   print_timer();
   return 0;
 }
